2f179fe08d05a4a5756c89126b759f9700ea8454
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-07-27 12:47:14 -0400

    [Add] - Flying Enemy

---
+++ Assets/02.Enemies/Bee/FlyingEnemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject player;
+
+    
+    [SerializeField] private Transform startingPoint;
+    public bool chase = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null)
+        {
+            return;
+        }
+        if(chase == true)
+        {
+            Chase();
+        }
+        else
+        {
+            StartingPoint();
+        }
+        Flip();
+    }
+
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, player.transform.position) <= 0.5f)
+        {
+            //Attack
+        }
+    }
+
+    private void StartingPoint()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);
+
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        if (transform.position.x > player.transform.position.x)
+        {
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+        else
+        {
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+        
+    }
+}
