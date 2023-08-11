bde9156312794d467351abb485746ac92cc974f2
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-08-11 03:26:15 -0400

    [Add] -- Game Over

---
+++ Assets/02.Project/01.Common/04.InteractiveObjects/Scripts/WinGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    [Header("Main Object")]
+    [SerializeField] private Transform visualTransform;
+
+    [Header("Layers & Tags")]
+    [SerializeField] private LayerMask InteractLayer;
+    private void Update()
+    {
+        if (Physics2D.OverlapCircle(visualTransform.position,  0, InteractLayer))
+        {
+            Application.LoadLevel("GameOver");
+        }
+    }
+}
