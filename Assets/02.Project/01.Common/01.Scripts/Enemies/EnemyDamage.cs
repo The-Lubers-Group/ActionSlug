e861b27e7b4522e78cc04be83b5095bc5a2ebd84
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-09-17 22:58:45 -0400

    [Fix] - Damage

--- Assets/02.Project/01.Common/01.Scripts/Enemies/EnemyDamage.cs
+++ Assets/02.Project/01.Common/01.Scripts/Enemies/EnemyDamage.cs
@@ -2,17 +2,27 @@ using LubyAdventure;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] protected int damage = 1;
+    private UnitController _player;
+
+
     
     protected void OnTriggerEnter2D(Collider2D collision)
     {
+        print("collision.gameObject.tag: " + collision.gameObject.tag);
+
         if (collision.gameObject.tag == "Player")
         {
-            print("Player");
-            collision.gameObject.GetComponent<UnitController>().PlayerHit(damage);
+            //collision.gameObject.GetComponent<UnitController>().PlayerHit(damage);
+            if (_player == null)
+            {
+                _player =  FindAnyObjectByType<UnitController>();
+            }
+            _player.PlayerHit(damage);
         }
     }
 }
