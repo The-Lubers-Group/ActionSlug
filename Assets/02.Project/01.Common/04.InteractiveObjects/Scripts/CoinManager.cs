a8dc25c4a77629ea8c5814767a8782a249f8ea89
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-09-21 18:35:23 -0400

    [add] - enemies

--- Assets/02.Project/01.Common/04.InteractiveObjects/Scripts/CoinManager.cs
+++ Assets/02.Project/01.Common/04.InteractiveObjects/Scripts/CoinManager.cs
@@ -6,35 +6,49 @@ using UnityEngine;
 public class CoinManager : MonoBehaviour
 {
     [Header("Main Object")]
-    [SerializeField] private Transform coin;
+    [SerializeField] private Transform _coin;
 
-    [Header("Layers & Tags")]
-    [SerializeField] private LayerMask interactLayer;
+    //[Header("Layers & Tags")]
+    //[SerializeField] private LayerMask interactLayer;
 
     [SerializeField] private AudioClip _soundFX;
     [SerializeField] private AudioSource _audioSource;
 
+    private bool _aux = false;
 
-    private SpriteRenderer spriteIcon;
+    private SpriteRenderer _spriteIcon;
     private void Start()
     {
-        spriteIcon = coin.GetChild(0).GetComponentInChildren<SpriteRenderer>();
+        _coin = transform.GetComponent<Transform>();
+        _spriteIcon = _coin.GetChild(0).GetComponentInChildren<SpriteRenderer>();
+
     }
+
+    private void Update()
+    {
+        if (_aux)
+        {
+            _coin.DOMove(MenuManager.main.UICoin.transform.position, 1);
+            StartCoroutine(Wait(0.5f));
+
+        }
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if((interactLayer.value & 1 << collision.gameObject.layer) == interactLayer.value)
+        if (collision.gameObject.tag == "Player")
         {
             _audioSource.PlayOneShot(_soundFX);
             MenuManager.main.AddCoin(1);
-            spriteIcon.sortingOrder = 3;
-            coin.DOMove(MenuManager.main.UICoin.transform.position, 1);
-            StartCoroutine(Wait(0.5f));
+            _spriteIcon.sortingOrder = 3;
+            _aux = true;
         }
     }
+
     IEnumerator Wait(float amt)
     {
         yield return new WaitForSeconds(amt);
-        spriteIcon.sortingOrder = 0;
-        Destroy(coin.gameObject);
+        _spriteIcon.sortingOrder = 0;
+        Destroy(_coin.gameObject);
     }
 }
