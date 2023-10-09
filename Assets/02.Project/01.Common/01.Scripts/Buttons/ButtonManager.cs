95aee24fcc04c1c2070c025e6078f356d72f31cd
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-10-09 16:03:09 -0400

    [Add] - New button

--- Assets/02.Project/01.Common/01.Scripts/Buttons/ButtonManager.cs
+++ Assets/02.Project/01.Common/01.Scripts/Buttons/ButtonManager.cs
@@ -1,15 +1,16 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class ButtonManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ButtonManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image _img;
-    [SerializeField] private Sprite _default, _press;
+    [SerializeField] private Sprite _default, _press, _hover;
     [SerializeField] private AudioClip _compressClip, _uncompressClip;
     [SerializeField] private AudioSource _source;
 
@@ -26,4 +27,18 @@ public class ButtonManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         _img.sprite = _default;
         _source.PlayOneShot(_uncompressClip);
     }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _img.sprite = _hover;
+        //transform.DOShakePosition(3.0f, strength: new Vector3(0, 4, 0), vibrato: 5, randomness: 1, snapping: false, fadeOut: true).OnComplete(() => _img.sprite = _default);
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+    }
+    
 }
